c6ce592a0653f0646aca1d2283f8c02fe1e3b97f
marcoadsl <marcoantnio.dasilvaleite@gmail.com>
2023-07-07 19:52:17 -0300

    Create prompt.js
    exercicios em java script

---
+++ prompt.js
@@ -0,0 +1,64 @@
+/*  
+    1. Usando o console do seu navegador ou terminal, apresente as informações das variaveis citadas abaixo:
+
+        Nome, idade, ano de nascimento, nacionalidade
+
+        Dica:
+        let IDENTIFICADOR = ATRIBUTO
+*/
+        let nome = "Isabela";
+        let idade = 25;
+        let anoDeNascimento = 1992;
+        let nacionalidade = "Brasileira"
+
+
+/*
+    2. Com as variaveis criadas, substitua os valores já definidos por valores diferentes atribuidos a ela:
+
+    Dica:
+    IDENTIFICADOR = ATRIBUTO
+
+    obs: Lembrando que queremos substituir o valor atribuido, portanto, não é necessario declarar a variavel duas vezes.
+*/
+    nome = "Maria";
+    idade = 40;
+    anoDeNascimento = 1975;
+    nacionalidade = "Francesa";
+
+
+/*
+    3.  Escreva uma variavel A que armazene o valor de 50 e valor de 20 em uma variavel B.
+        Armazene na variavel C  o valor de A  + valor de B
+        Mostre o valor de A, B e C
+*/
+
+        let A = 50;
+        let B = 20;
+        let C = A + B;
+        console.log(A,B,C); // 50 20 70
+
+
+/*
+    4.  Criar uma constante A que armazene um valor de 20 e  troque o valor de A para 50.
+        Responda:
+        Essa operação funcionou ? justifique
+*/
+        const A = 20;
+        A = 50; // Tipe de erro: atribuição para a varivel constante.
+
+        // Resposta: ????
+
+/*
+
+a) Crie as seguintes variáveis: nome, formacao, hobby, idade, altura, anoNascimento.
+
+b) Atribua valores diferentes às variáveis criadas no exercício anterior.
+
+c) Atribua novos valores de TIPO diferente para as variáveis e mostre-os novamente.
+
+d) Mostre no console os nomes, os valores e os tipos de valores de cada uma das variáveis
+
+
+Obs.: Typeof retorna o tipo de um valor qualquer.
+
+*/
